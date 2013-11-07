fb5db060fb081c2e0592e81c22274e3ec397be73
Chris Smith <christopher_j.smith@tufts.edu>
2013-11-07 18:42:18 -0500

    added ability to adjust camera settings

--- rosCamWrap.h
+++ rosCamWrap.h
@@ -16,6 +16,22 @@
 
 namespace enc = sensor_msgs::image_encodings;
 
+#ifndef BAXTER_CAMERA_CONTROL
+#define BAXTER_CAMERA_CONTROL
+
+#define CAMERA_CONTROL_EXPOSURE baxter_msgs::CameraControl::CAMERA_CONTROL_EXPOSURE
+#define CAMERA_CONTROL_GAIN baxter_msgs::CameraControl::CAMERA_CONTROL_GAIN
+#define CAMERA_CONTROL_WHITE_BALANCE_R baxter_msgs::CameraControl::CAMERA_CONTROL_WHITE_BALANCE_R
+#define CAMERA_CONTROL_WHITE_BALANCE_G baxter_msgs::CameraControl::CAMERA_CONTROL_WHITE_BALANCE_G
+#define CAMERA_CONTROL_WHITE_BALANCE_B baxter_msgs::CameraControl::CAMERA_CONTROL_WHITE_BALANCE_B
+#define CAMERA_CONTROL_WINDOW_X baxter_msgs::CameraControl::CAMERA_CONTROL_WINDOW_X
+#define CAMERA_CONTROL_WINDOW_Y baxter_msgs::CameraControl::CAMERA_CONTROL_WINDOW_Y
+#define CAMERA_CONTROL_FLIP baxter_msgs::CameraControl::CAMERA_CONTROL_FLIP
+#define CAMERA_CONTROL_MIRROR baxter_msgs::CameraControl::CAMERA_CONTROL_MIRROR
+#define CAMERA_CONTROL_RESOLUTION_HALF baxter_msgs::CameraControl::CAMERA_CONTROL_RESOLUTION_HALF
+
+#endif
+
 cv::Mat scene;
 cv::Mat templ;
 
@@ -37,7 +53,7 @@ public:
     
     //subscribes through provided node handle, to the camera name provided
     //with the perscribed buffer
-    BaxterCamera(ros::NodeHandle,std::string,uint);
+    BaxterCamera(std::string,uint);
     BaxterCamera(std::string);
     ~BaxterCamera();
     
@@ -52,7 +68,31 @@ public:
     unsigned char is_bigendian();
     std::vector<unsigned char> data();
     std::string name();
+    
+    //Adjust CameraSettings
     Resolution resolution();
+    void resolution(Resolution);
+    int fps();
+    void fps(int);
+    int exposure();
+    void exposure(int);
+    int gain();
+    void gain(int);
+    int white_balance_red();
+    void white_balance_red(int);
+    int white_balance_green();
+    void white_balance_green(int);
+    int white_balance_blue();
+    void white_balance_blue(int);
+    Resolution window();
+    void window(Resolution);
+    bool flip();
+    void flip(bool);
+    bool mirror();
+    void mirror(bool);
+    bool half_resolution();
+    void half_resolution(bool);
+    void reset_controls();
     
     //returns the entire ROS image message
     sensor_msgs::Image image();
@@ -61,10 +101,10 @@ public:
     void display(bool);    // sets _show to true || false
     //void TimerCallback(const ros::TimerEvent &);  // custom timer callback for this object. Edit as required
     
+    // Camera Services
     std::vector<std::string> list_cameras();
     int close();
-    int open(Resolution);
-    int resize(Resolution);               //closes then opens the camera with resolution
+    int open();
     /*returns an opencv formatted image
     *   Check that image exists before working with it
     *           Mat image = cam.cvImage();
@@ -74,9 +114,13 @@ public:
     
 private:
     sensor_msgs::Image _img;
+    baxter_msgs::CameraSettings _settings;
     std::string WINDOW; //name of window when displaying image through opencv
     unsigned int _bufferSize; //size of subscriber buffer
     ros::Subscriber _sub; //subscriber
+    ros::ServiceClient _list_client;
+    ros::ServiceClient _close_client;
+    ros::ServiceClient _open_client;
     bool _show; //when true, _callback will display the image
     std::string _name; //name of camera
     ros::NodeHandle _nh; //node handle used to setup subscriber if one is not provided
@@ -85,6 +129,9 @@ private:
     //Subscription callback that allocates message data to 
     //the corresponding private variables
     void _callback(const sensor_msgs::Image::ConstPtr&);
+    int _reload();               //closes then opens the camera with _settings
+    int _get_control_value(int, int);   // control id, default
+    void _set_control_value(int, int);  // control id, value
     
     /*performs conversion from ROS to opencv image.
     *  CvImagePtr{ std_msgs::Header header;
@@ -107,11 +154,10 @@ BaxterCamera::~BaxterCamera()
     //cv::destroyAllWindows();
 }
 
-BaxterCamera::BaxterCamera(ros::NodeHandle nh, std::string name, uint bufferSize)
+BaxterCamera::BaxterCamera(std::string name, uint bufferSize)
 {
     //Name must be full name, e.g left_hand_camera, right_hand_camera
     _name = name;
-    _nh = nh;
     _bufferSize = bufferSize;
     Init();    
 }
@@ -131,6 +177,12 @@ void BaxterCamera::Init()
     cv::namedWindow(WINDOW, CV_WINDOW_AUTOSIZE);
     _img.height = 0;
     _sub = _nh.subscribe(topic, _bufferSize, &BaxterCamera::_callback, this);
+    _list_client = _nh.serviceClient<baxter_msgs::ListCameras>("/cameras/list");
+    _close_client = _nh.serviceClient<baxter_msgs::CloseCamera>("/cameras/close");
+    _open_client = _nh.serviceClient<baxter_msgs::OpenCamera>("/cameras/open");
+    _settings.width = 320;
+    _settings.height = 200;
+    _settings.fps = 20;
     ros::Duration timeout(2.0);
     ros::Time begin = ros::Time::now();
     //wait for an image or timeout
@@ -167,6 +219,39 @@ void BaxterCamera::_callback(const sensor_msgs::Image::ConstPtr& msg)
     
 }
 
+int BaxterCamera::_get_control_value(int control_id, int defaul)
+{
+    int index = -1;
+    for(int i = 0; i < _settings.controls.size(); i++)
+    {
+        if (_settings.controls[i].id == control_id){
+            index = i; break;
+        }
+    }
+    if (index > -1)
+        return _settings.controls[index].value;
+    return defaul;
+}
+
+void BaxterCamera::_set_control_value(int control_id, int val)
+{
+    int index = -1;
+    for(int i = 0; i < _settings.controls.size(); i++)
+    {
+        if(_settings.controls[i].id == control_id){
+            index = i; break;
+        }
+    }
+    if(index > -1){
+        _settings.controls[index].value = val;
+        return;
+    }
+    baxter_msgs::CameraControl ctl;
+    ctl.id = control_id;
+    ctl.value = val;
+    _settings.controls.push_back(ctl);
+}
+
 cv_bridge::CvImagePtr BaxterCamera::_getCvImage()
 {
     cv_bridge::CvImagePtr temp;
@@ -195,40 +280,38 @@ cv::Mat BaxterCamera::cvImage()
 
 std::vector<std::string> BaxterCamera::list_cameras()
 {
-    std::string serv = "/cameras/list";
-    ros::ServiceClient client = _nh.serviceClient<baxter_msgs::ListCameras>(serv);
     baxter_msgs::ListCameras srv;
-    if(client.call(srv))
+    if(_list_client.call(srv))
         return srv.response.cameras;
 }
 
 int BaxterCamera::close()
 {
-    std::string serv = "/cameras/close";
-    ros::ServiceClient client = _nh.serviceClient<baxter_msgs::CloseCamera>(serv);
     baxter_msgs::CloseCamera srv;
     srv.request.name = _name;
-    if(client.call(srv))
+    if(_close_client.call(srv))
         return srv.response.err;
 }
 
-int BaxterCamera::open(Resolution res)
+int BaxterCamera::open()
 {
-    std::string serv = "/cameras/open";
-    ros::ServiceClient client = _nh.serviceClient<baxter_msgs::OpenCamera>(serv);
     baxter_msgs::OpenCamera srv;
     srv.request.name = _name;
-    srv.request.settings.width = res.width;
-    srv.request.settings.height = res.height;
-    if(client.call(srv))
+    if (_name == "head_camera")
+    {
+        this->_set_control_value(CAMERA_CONTROL_FLIP, true);
+        this->_set_control_value(CAMERA_CONTROL_MIRROR, true);
+    }
+    srv.request.settings = _settings;
+    if(_open_client.call(srv))
         return srv.response.err;
     
 }
 
-int BaxterCamera::resize(Resolution res)
+int BaxterCamera::_reload()
 {
     this->close();
-    return this->open(res);
+    return this->open();
 }
 
 void BaxterCamera::display()
@@ -247,9 +330,172 @@ Resolution BaxterCamera::resolution()
     return Resolution(_img.width,_img.height);
 }
 
+void BaxterCamera::resolution(Resolution res)
+{
+    this->_settings.width = res.width;
+    this->_settings.height = res.height;
+    this->_reload();
+}
+
+int BaxterCamera::fps()
+{
+    return this->_settings.fps;
+}
+
+void BaxterCamera::fps(int val)
+{
+    this->_settings.fps = val;
+}
+
+int BaxterCamera::exposure()
+{
+    return _get_control_value(CAMERA_CONTROL_EXPOSURE, -1);
+}
+
+void BaxterCamera::exposure(int val)
+{
+    if ((val < 0 || val > 100) && val != -1){
+        throw(std::out_of_range("Invalid exposure value"));
+        return;
+    }
+    this->_set_control_value(CAMERA_CONTROL_EXPOSURE, val);
+    this->_reload();
+}
+
+int BaxterCamera::gain()
+{
+    return _get_control_value(CAMERA_CONTROL_GAIN, -1);
+}
+
+void BaxterCamera::gain(int val)
+{
+    if ((val < 0 || val > 79) && val != -1){
+        throw(std::out_of_range("Invalid gain value"));
+        return;
+    }
+    this->_set_control_value(CAMERA_CONTROL_GAIN, val);
+    this->_reload();
+}
+
+int BaxterCamera::white_balance_red()
+{
+    return this->_get_control_value(CAMERA_CONTROL_WHITE_BALANCE_R, -1);
+}
+
+void BaxterCamera::white_balance_red(int val)
+{
+    if ((val < 0 || val > 4095) && val != -1){
+        throw(std::out_of_range("Invalid white balance value"));
+        return;
+    }
+    this->_set_control_value(CAMERA_CONTROL_WHITE_BALANCE_R, val);
+    this->_reload();
+}
+
+int BaxterCamera::white_balance_green()
+{
+    return this->_get_control_value(CAMERA_CONTROL_WHITE_BALANCE_G, -1);
+}
+
+void BaxterCamera::white_balance_green(int val)
+{
+    if ((val < 0 || val > 4095) && val != -1){
+        throw(std::out_of_range("Invalid white balance value"));
+        return;
+    }
+    this->_set_control_value(CAMERA_CONTROL_WHITE_BALANCE_G, val);
+    this->_reload();
+}
+int BaxterCamera::white_balance_blue()
+{
+    return this->_get_control_value(CAMERA_CONTROL_WHITE_BALANCE_B, -1);
+}
+void BaxterCamera::white_balance_blue(int val)
+{
+    if ((val < 0 || val > 4095) && val != -1){
+        throw(std::out_of_range("Invalid white balance value"));
+        return;
+    }
+    this->_set_control_value(CAMERA_CONTROL_WHITE_BALANCE_B, val);
+    this->_reload();
+}
+Resolution BaxterCamera::window()
+{
+    int x = this->_get_control_value(CAMERA_CONTROL_WINDOW_X, -1);
+    if (x == -1){
+        if(this->half_resolution())
+            return Resolution(x/2, this->resolution().height);
+        else
+            return this->resolution();
+    }
+    else
+        return Resolution(x,this->_get_control_value(CAMERA_CONTROL_WINDOW_Y, -1));    
+}
+void BaxterCamera::window(Resolution res)
+{
+    /*
+     * Set Camera window. The max size is a function of the current camera
+     * resolution and if half_resolution is enabled or not.
+     */
+    int x = res.width;
+    int y = res.height;
+    int limit_x = 1280 - this->resolution().width;
+    int limit_y = 800 - this->resolution().height;
+    
+    if (this->half_resolution()){
+        limit_x /= 2;
+        limit_y /= 2;
+    }
+    
+    std::stringstream err_x, err_y;
+    err_x<<"Max X window is "<<limit_x;
+    err_y<<"Max y window is "<<limit_y;
+    if (x < 0 || x > limit_x)
+        throw(std::out_of_range(err_x.str()));
+    if (y < 0 || y > limit_y)
+        throw(std::out_of_range(err_y.str()));
+    
+    this->_set_control_value(CAMERA_CONTROL_WINDOW_X, x);
+    this->_set_control_value(CAMERA_CONTROL_WINDOW_Y, y);
+    this->_reload();
+}
+
+bool BaxterCamera::flip()
+{
+    return this->_get_control_value(CAMERA_CONTROL_FLIP, false);
+}
+void BaxterCamera::flip(bool val)
+{
+    this->_set_control_value(CAMERA_CONTROL_FLIP, val);
+    this->_reload();
+}
+bool BaxterCamera::mirror()
+{
+    return this->_get_control_value(CAMERA_CONTROL_MIRROR, false);
+}
+void BaxterCamera::mirror(bool val)
+{
+    this->_set_control_value(CAMERA_CONTROL_MIRROR, val);
+    this->_reload();
+}
+bool BaxterCamera::half_resolution()
+{
+    return this->_get_control_value(CAMERA_CONTROL_RESOLUTION_HALF, false);
+}
+void BaxterCamera::half_resolution(bool val)
+{
+    this->_set_control_value(CAMERA_CONTROL_RESOLUTION_HALF, val);
+    this->_reload();
+}
+
+void BaxterCamera::reset_controls()
+{
+    this->_settings.controls.clear();
+}
+
 sensor_msgs::Image BaxterCamera::image()
 {
-    return _img;
+    return this->_img;
 }
 
 std::vector<unsigned char> BaxterCamera::data()
